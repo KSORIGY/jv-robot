98cb093d13d9e1a70eb3f8678d03ff64a0c11087
DenysHirniak <denforever777@gmail.com>
2025-07-30 15:11:41 +0300

    checkstyle fixes robot-hw

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,33 +2,33 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      if (robot.getX() < toX) {
+        if (robot.getX() < toX) {
           rotateRobotTo(robot, Direction.RIGHT);
-          while (robot.getX() < toX) {
+            while (robot.getX() < toX) {
               robot.stepForward();
-          }
-      } else if (robot.getX() > toX) {
-          rotateRobotTo(robot, Direction.LEFT);
-          while (robot.getX() > toX) {
+            }
+        } else if (robot.getX() > toX) {
+            rotateRobotTo(robot, Direction.LEFT);
+            while (robot.getX() > toX) {
               robot.stepForward();
-          }
-      }
-      if (robot.getY() < toY) {
+            }
+        }
+        if (robot.getY() < toY) {
           rotateRobotTo(robot, Direction.UP);
-          while (robot.getY() < toY) {
+            while (robot.getY() < toY) {
               robot.stepForward();
-          }
-      } else if (robot.getY() > toY) {
+            }
+        } else if (robot.getY() > toY) {
           rotateRobotTo(robot, Direction.DOWN);
-          while (robot.getY() > toY) {
+            while (robot.getY() > toY) {
               robot.stepForward();
-          }
-      }
+            }
+        }
     }
 
     private void rotateRobotTo(Robot robot, Direction targetDirection) {
-            while (robot.getDirection() != targetDirection) {
-                robot.turnRight();
-            }
+        while (robot.getDirection() != targetDirection) {
+            robot.turnRight();
+        }
     }
 }
